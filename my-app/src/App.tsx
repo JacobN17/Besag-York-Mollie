9d7afac5e8e3e510bc9420b3cf51ecf53582386f
Daniel Antonelli <danielantonelli@Daniels-MBP.fios-router.home>
2020-05-12 08:15:48 -0700

    start stying for navbar

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,53 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import React from 'react';
-import logo from './logo.svg';
+import { Navbar } from './components/Navbar';
+import './components/Navbar.css';
 import './App.css';
 
-function App() {
+const navigation: Array<NavLink> = [
+  {name: 'BYM', path: './home'}, 
+  {name: 'Upload Data', path:'./upload'},
+  {name: 'Share', path: './share'}
+];
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container header-container">
+      <div className="app-header">
+        <Navbar link={navigation[0]} />
+        <Navbar link={navigation[1]} />
+        <Navbar link={navigation[2]} />
+      </div>
     </div>
-  );
+    
+  )
 }
 
 export default App;
