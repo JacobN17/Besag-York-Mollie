2515b1c3fd5bc1b824d4f3922d328866c0b8c6f0
Daniel Antonelli <danielantonelli@Daniels-MBP.fios-router.home>
2020-05-12 08:12:20 -0700

    start stying for navbar

---
+++ my-app/src/typed.d.ts
@@ -0,0 +1,8 @@
+// types.d.ts makes it s declaration file
+// no need to constantly import/export new types to each file.
+// ts compiler will handle this file
+
+type NavLink = {
+    name: string;
+    path: string
+};
