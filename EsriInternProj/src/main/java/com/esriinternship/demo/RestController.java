9b1646fd743f0f6d9d6db16c2040ee3195330ee2
RigobertoVincent <Rigobertovicente.Gonzalez@calbaptist.edu>
2020-05-25 15:44:37 -0700

    pulled and checking out to rigo branch

---
+++ EsriInternProj/src/main/java/com/esriinternship/demo/RestController.java
@@ -0,0 +1,17 @@
+package com.esriinternship.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.FileUtils;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class RestController {
+
+
+}
