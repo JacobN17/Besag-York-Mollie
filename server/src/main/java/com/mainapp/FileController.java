14e0fa64d0b08d32d751d7dc5a9688a87df82a8f
Cameron Warren <cameronjoseph.warren@calbaptist.edu>
2020-06-04 18:35:58 -0700

    Database connection

--- server/src/main/java/com/mainapp/FileController.java
+++ server/src/main/java/com/mainapp/FileController.java
@@ -1,16 +1,14 @@
-package com.mainapp.controller;
+package com.mainapp;
 
-import com.mainapp.model.File;
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import javax.xml.transform.Result;
+import java.sql.*;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -19,10 +17,57 @@ import java.util.List;
 @RestController
 //@CrossOrigin("http://localhost:3000") // Allow requests from specific origins
 public class FileController {
-        @RequestMapping(method = RequestMethod.POST, consumes = "multipart/form-data")
-        public void GetFile(@RequestParam("file") MultipartFile file) {
-            System.out.println(file.getSize());
+
+    //Establishes connection to database
+    private Connection connect() {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/postgres",
+                            "postgres", "root");
+            System.out.println("Opened database successfully");
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
+        return c;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = "multipart/form-data")
+    public void GetFile(@RequestParam("file") MultipartFile file) throws SQLException {
+        System.out.println(file.getSize());
+        Connection c = connect();
+        Statement s = null;
+        try {
+            s = c.createStatement();
+            ResultSet result = s.executeQuery("SELECT * FROM public.\"Order_T\"");
+            System.out.println(getResults(result, "%-21s"));
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+        } finally {
+            s.close();
+            c.close();
+        }
+    }
+
+    //Formats results of queries
+    private String getResults(ResultSet resultSet, String format) throws SQLException {
+        StringBuilder results = new StringBuilder();
+        ResultSetMetaData metaData = resultSet.getMetaData();
+
+        for (int i = 0; i < metaData.getColumnCount(); i++) {
+            results.append(String.format(format,metaData.getColumnName(i+1)));
+        }
+
+        while (resultSet.next()) {
+            for (int i = 0; i < metaData.getColumnCount(); i++) {
+                results.append(String.format(format,resultSet.getString(i+1)));
+            }
+        }
+
+        return results.toString();
+    }
 }
 
 
