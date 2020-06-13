d1380391c96862e8817dfefffdc36a3d6a7a6b82
aaron <aaronyanes92@gmail.com>
2020-06-13 15:17:24 -0700

    download feature

--- frontend/src/components/jsonToCSV.tsx
+++ frontend/src/components/jsonToCSV.tsx
@@ -2,17 +2,23 @@ import React, { useState } from 'react';
 import '../css/home.css';
 import '../css/converter.css';
 
-function toCsvFormat(str: string): string[] {
+let csvFields = function toCsvFormat(str: string): string {
     const { Parser } = require('json2csv');
     let fields: string[] = [];
-    
-    let obj: JSON = JSON.parse(str);
-    Object.entries(obj).forEach(([key,value]) => {
-        for (let v in value) {
-            fields.push(v);
+    if (str) {
+        try {
+            let obj: JSON = JSON.parse(str);
+            Object.entries(obj).forEach(([key,value]) => {
+                for (let v in value) {
+                    fields.push(v);
+                }
+            })
+        } catch(e) {
+            let area = document.getElementById('json') as HTMLInputElement;
+            area.innerHTML = "Please check JSON formatting";
         }
-    })
-    
+    }
+     
     // filter here
     fields = fields.filter((item, index) => {
         return fields.indexOf(item) === index;
@@ -26,35 +32,76 @@ function toCsvFormat(str: string): string[] {
         let area = document.getElementById('json') as HTMLInputElement;
         area.innerHTML = csv;
     } catch (err) {
-        //TODO: post error on front end
-      console.error(err);
+        // error
     }
 
-    return fields;
+    return str;
 }
 
+
 // this should trigger if they choose to download
-function downloadCSV(csvFields: string[]) {
+function downloadCSV() {
+    const { Parser } = require('json2csv');
+    let textArea = document.getElementById('input') as HTMLInputElement;
+    let data = textArea.value;
+    let fields: string[] = [];
+    if (data) {
+        try {
+            const objToJson: JSON = JSON.parse(data);
+            Object.entries(objToJson).forEach(([key,value]) => {
+                for (let v in value) {
+                    fields.push(v);
+                }
+            })
+        } catch (e) {
+            // error
+            console.log("data to json failed");
+        }
+    }
+
+    fields = fields.filter((item, index) => {
+        return fields.indexOf(item) === index;
+    })
+
+    const opts = { fields };
+    try {
+        const parser = new Parser(opts);
+        const csv = parser.parse(JSON.parse(data));   
+        download(csv);   
+    } catch (err) {
+        // error
+    }
     
 }
 
 
+// dl function
+function download(data: string) {
+    const blob = new Blob([data], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob); //creates url version of csv
+    const a = document.createElement('a');
+    a.setAttribute('hidden', '');
+    a.setAttribute('href',url);
+    a.setAttribute('download', 'download.csv');
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
 
 
-export const Converter: React.FC = () => {
-    const [textArea, setTextArea] = useState("");
 
+export const Converter: React.FC = () => {    
     return (
         <div className="input-container">
             <div className="form-wrapper">
                 <label>take in json</label>
                 <br />
-                <textarea className="json-area" onChange={event => toCsvFormat(event.target.value)}></textarea>
+                <textarea id="input" className="json-area" onChange={event => csvFields(event.target.value)}></textarea>
                 <br />
                 <label>output csv file to download</label>
                 <br />
                 <textarea id="json" className="json-output"></textarea>
-                <button>download csv</button>
+                <button onClick={downloadCSV}>download csv</button>
             </div>
         </div>
     )
